da2fc4da99d873a5a915ef9c20052935d0c0c487
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-07-02 11:35:56 +0200

    Working App

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,13 +2,17 @@ import React from 'react';
 import styles from './Hero.scss';
 
 class Hero extends React.Component {
-    <header className={styles.component}>
-        <h2 className={styles.title}>Things to do</h2>
-        <img className={styles.image}
-            src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" 
-            alt="image"
-        />
-    />
+    render() {
+        return (
+            <header className={styles.component}>
+                <h2 className={styles.title}>Things to do</h2>
+                <img className={styles.image}
+                    src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
+                    alt="image"
+                />
+            </header>
+        );
+    }
 }
 
 export default Hero;
