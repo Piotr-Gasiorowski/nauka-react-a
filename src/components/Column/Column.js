9477cc1d65f3293e295f76a14ed831b5ffec1860
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-07-09 08:58:40 +0200

    Initial commit

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,7 +5,7 @@ class Column extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h3 className={styles.title}>My first React app</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
       </main>
     )
   }
