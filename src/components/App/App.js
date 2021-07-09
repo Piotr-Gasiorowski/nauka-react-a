8523f4241d66ece98535bd697c3348638da91b7a
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-07-09 08:57:40 +0200

    Add image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,12 +9,9 @@ class App extends React.Component {
         <main className={styles.component}>
           <h1 className={styles.title}>My first React app</h1>
           <h2 className={styles.subtitle}>Hello World!</h2>
-          <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
-            alt="image"
+          <List title={['Things to do ', <sup key='1'>soon!</sup>]} 
+            image="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" 
           />
-          <List title />
-          <List image />
-        
         </main>  
       </div>
     )
