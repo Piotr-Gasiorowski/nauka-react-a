8394f16e69c10d84a1aa4a7ee57fea194296c4fb
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-02 18:37:00 +0200

    Add renders

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column.js';
+import Column from '../Column/ColumnContainer.js';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+// import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   state = {
@@ -21,38 +21,25 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   }
 
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.lenght ? state.columns[state.columns.lenght-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const {title, image, description, columns} = this.props;
     return (
       <div id="List">
         <section className={styles.component}>
-          <Hero titleText={this.props.title} image={this.props.image} />
+          <Hero titleText={title} image={image} />
           <div className={styles.description}>
-            {ReactHtmlParser(this.props.description)}
+            {ReactHtmlParser(description)}
           </div>
           <div className={styles.columns}>
-            {this.state.columns.map(({key, ...columnProps}) => (
-              <Column key={key} {...columnProps} />
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
             ))}
           </div>
+          {/*
           <div className={styles.creator}>
             <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>
+            */}
         </section>  
       </div>
     );
