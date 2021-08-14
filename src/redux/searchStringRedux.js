f99f241091cb0ee5568a5b6efd79e44661780691
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-14 11:06:42 +0200

    Add countAllCards and countVisibleCards

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,24 +1,24 @@
 // selectors
 export const getSearchString = ({searchString}) => searchString;
-export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).lenght;
+export const countAllCards = ({cards}) => cards.length;
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
-const reducerName = 'searchString';
+const reducerName = 'search';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
-export const CHANGE = createActionName('CHANGE');
+export const CHANGE_SEARCH = createActionName('CHANGE_SEARCH');
 
 // action creators
-export const createActionChange = payload => ({ payload, type: CHANGE });
+export const createAction_changeSearchString = payload => ({ payload, type: CHANGE_SEARCH });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
-    case CHANGE:
-      return [...statePart, action.payload];
+    case CHANGE_SEARCH:
+      return  action.payload;
     default:
       return statePart;
   }
 }
-
