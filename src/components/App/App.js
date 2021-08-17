b3544766032d9a1d39e0c25e1934842f5601dda3
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-17 19:20:52 +0200

    Repaired

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,14 @@ import Info from '../Info/Info';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 
-<BrowserRouter>
-  <MainLayout>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/info' component={Info} />
-    </Switch>
-  </MainLayout>;
-</BrowserRouter>;
+const App = ()=>
+  <BrowserRouter>
+    <MainLayout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </MainLayout>;
+  </BrowserRouter>;
+
+export default App;
