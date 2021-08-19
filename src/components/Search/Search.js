83d2c0244e045c072ab74f5697aadfa63232d22a
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-19 14:18:39 +0200

    Add react-router

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 import Container from '../Container/Container';
+import {withRouter} from 'react-router';
 
 class Search extends React.Component {
   static propTypes = {
@@ -13,12 +14,13 @@ class Search extends React.Component {
     changeSearchString: PropTypes.func,
     countVisible: PropTypes.number,
     countAll: PropTypes.number,
+    history: PropTypes.object,
   }
 
   static defaultProps = {
     text: settings.search.defaultText,
   }
-
+  
   state = {
     value: this.props.searchString,
   }
@@ -31,11 +33,11 @@ class Search extends React.Component {
   }
 
   handleOK(){
-    this.props.changeSearchString(this.state.value);
+    this.props.history.push(`/search/${this.state.value}`);
   }
 
-  componentDidUpdate(prevProps){
-    if(this.props.searchString != prevProps.searchString){
+  componentDidUpdate(prevProps) {
+    if(this.props.searchString != prevProps.searchString) {
       this.setState({value: this.props.searchString});
     }
   }
@@ -57,7 +59,7 @@ class Search extends React.Component {
             <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
           </div>
           <div>
-            { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
+            { countVisible == countAll ? '' : `${countVisible} / ${countAll}`}
           </div>
         </div>
       </Container>
@@ -65,4 +67,4 @@ class Search extends React.Component {
   }
 }
 
-export default Search;
+export default withRouter(Search);
