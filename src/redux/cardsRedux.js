14b6af39245e649f2b9b7650a34d33f9f4495ab1
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-19 14:21:11 +0200

    Add getFilteredCards

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,14 +1,16 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForList = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
-export const getSearchCardResult = ({cards, searchString}) => cards.filter (card => new RegExp(searchString, 'i').test(card.title));
+export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getFilteredCards = ({cards, searchString}) => cards.filter (card => new RegExp(searchString, 'i').test(card.title));
+
 // action name creator
 const reducerName = 'cards';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const ADD_CARD = createActionName('ADD_CARD');
+export const REMOVE_CARD = createActionName('REMOVE_CARD');
 
 // action creators
 export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
@@ -18,6 +20,9 @@ export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
     case ADD_CARD:
       return [...statePart, action.payload];
+    case REMOVE_CARD:
+      statePart = statePart.cards.filter((card) => card.id !== action.payload.id);
+      return [...statePart];
     default:
       return statePart;
   }
