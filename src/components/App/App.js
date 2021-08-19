5b917738a5ce37d16fe46013a93b2c7b6be4d541
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-19 14:11:42 +0200

    Add SearchResult

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,9 @@ import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import List from '../List/ListContainer';
+import FAQ from '../FAQ/FAQ';
+import SearchResults from '../SearchResults/SearchResultsContainer.js';
 
 const App = ()=>
   <BrowserRouter>
@@ -10,6 +13,9 @@ const App = ()=>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
+        <Route exact path='/faq' component={FAQ} />
+        <Route exact path='/list/:id' component={List} />
+        <Route exact path='/search/:cards' component={SearchResults} />
       </Switch>
     </MainLayout>;
   </BrowserRouter>;
