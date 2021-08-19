e9cf3b1d311ee9f4948c155da977dc43377ee7b7
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-19 14:23:38 +0200

    Initial commit

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './SearchResults.scss';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import {settings} from '../../data/dataStore.js';
+import Icon from '../Icon/Icon.js';
+import Container from '../Container/Container';
+
+class SearchResults extends React.Component {
+  
+  static propTypes = {
+    name: PropTypes.string,
+    cards: PropTypes.array,
+    title: PropTypes.string,
+    icon: PropTypes.string,
+    changeSearchString: PropTypes.func,
+    searchString: PropTypes.string,
+    match: PropTypes.object,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
+
+  componentDidMount () {
+    this.props.changeSearchString(this.props.match.params.searchString);
+  }
+
+  componentDidUpdate () {
+    this.props.changeSearchString(this.props.match.params.searchString);
+  }
+
+  render() {
+    const {cards} = this.props;
+    return (
+      <Container>
+        <section className={styles.component}>
+          <h3 className={styles.title}>{this.props.name} {this.props.title}
+            <span className={styles.icon}>
+              <Icon name={this.props.icon} />
+            </span>
+          </h3>
+          <div className={styles.columns}>
+            {cards.map (cardData => (
+              <Card key={cardData.title} {...cardData} />
+            ))}
+          </div>
+        </section>
+      </Container>
+    );
+  }
+}
+
+export default SearchResults;
---
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import SearchResults from './SearchResults.js';
+import { getFilteredCards } from '../../redux/cardsRedux.js';
+import {createAction_changeSearchString} from '../../redux/searchStringRedux';
+
+const mapStateToProps = (state) => ({
+  cards: getFilteredCards(state),
+  columns: state.columns,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
