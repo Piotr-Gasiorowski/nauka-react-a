d49b19ed7945a1386fb18a95f64f1092c38f05db
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-08-19 14:19:37 +0200

    Add header

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -80,6 +80,18 @@ const lists = [
     description: 'Interesting things I want to check out!',
     image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   },
+  {
+    id: 'list-2',
+    title: 'Places to see <sup>soon!</sup>',
+    description: 'Interesting places I want to visit!',
+    image: 'https://images.pexels.com/photos/4636243/pexels-photo-4636243.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    id: 'list-3',
+    title: 'Cars to drive <sup>soon!</sup>',
+    description: 'Beautiful cars I want to drive!',
+    image: 'https://images.pexels.com/photos/1383834/pexels-photo-1383834.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
 ];
 
 const columns = [
@@ -156,6 +168,24 @@ export const FAQData = {
   image: 'https://images.pexels.com/photos/3791466/pexels-photo-3791466.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   imageTitle: 'My place on Earth',
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+};
+
+export const header = {
+  logo:'user-astronaut',
+  nav: [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'INFO',
+      path: '/info',
+    },
+    {
+      name: 'FAQ',
+      path: '/faq',
+    },
+  ],
 };
 
 const initialStoreData = {
